054692367da9041233624c7d1e275bf0c2d7f870
慧 中浜 <keishotbow@gmail.com>
2019-12-20 14:27:50 +0900

    add new file

--- main.cpp
+++ main.cpp
@@ -12,20 +12,23 @@ int menu_no_get(void) {
 	return atoi(buff);
 }
 
+// メイン関数
 int main(void) {
 	Gakusei gakusei[MAX_REC];
 	int cunt = 1;
 
 	// この時点でGakuseiに情報が入力されている
-	gakusei[0].gakuno = 111;
+	// ファイル入出力(ファイルがないので省略)
+	gakusei[0].gakuno = 111; // 適当に学籍番号入れた
 
-	// 表示か検索か終了か選ぶ
+	// メインメニューへ
 	g_main_menu(gakusei, cunt);
 	
 	system("pause");
 	return 0;
 }
 
+// メインメニュー
 void g_main_menu(Gakusei * gakusei, int cunt) {
 	int ret = 0;
 	while (ret != 9) {
@@ -53,75 +56,106 @@ void g_main_menu(Gakusei * gakusei, int cunt) {
 	}
 }
 
+// 学生情報を表示する
 void g_out_menu(Gakusei *gakusei, int cunt) {
-	printf("レコード内の全学生情報を表示します\n");
-	printf("学生情報のヘッダーを入力してください：");
+	printf("＜＜＜学生情報　表示処理＞＞＞");
+	printf("　　　　学籍番号順表示：１\n");
+	printf("　　　　氏名カナ順表示：２\n");
+	printf("　　　　郵便番号準表示：３\n");
+	printf("　メインメニューに戻る：９\n");
+	printf("処理番号を選択してください：");
+	int num;
+	while (scanf_s("%d", &num) != '9') {
+		switch (num) {
+		case 1:
+			sort_gakusei_bubble(num, gakusei, cunt);
+			break;
+		case 2:
+			sort_gakusei_bubble(num, gakusei, cunt);
+			break;
+		case 3:
+			sort_gakusei_bubble(num, gakusei, cunt);
+			break;
+		case 9:
+			return;
+			break;
+		default:
+			break;
+		}
+	}
+
 	int i = 0;
-	char title[100] = "";
-	scanf_s("%s", &title, 100);
+	char title[100] = "＜＜＜学生情報一覧＞＞＞";
+	//scanf_s("%s", &title, 100);
 	disp_gakusei(title, gakusei, cunt);
 }
 
+// 学生情報を検索する
 void g_srch_menu(Gakusei *gakusei, int cunt) {
-	printf("レコード内の学生情報を検索します\n");
 
-	printf("キー識別番号を選択してください。\n");
+	printf("＜＜＜学生情報　検索処理メニュー＞＞＞\n");
 	printf("------------識別番号一覧------------\n");
-	printf("１：学籍番号で探索\n");
-	printf("２：性カナで探索\n");
-	printf("３：名カナで探索\n");
-	printf("４：性カナ、名カナで探索\n");
-	printf("５：郵便番号(前3桁)で探索\n");
-	printf("６：郵便番号(全桁)で探索\n");
-	printf("７：都道府県カナで探索\n");
+	printf("１：学籍番号で検索\n");
+	printf("２：郵便番号(前3桁)で検索\n");
+	printf("３：郵便番号(全7桁)で検索\n");
+	printf("４：氏名カナ(姓)で検索\n");
+	printf("５：氏名カナ(姓名)で検索\n");
+	printf("６：都道府県カナで検索\n");
+	printf("７：住所カナ(○○から始まる)で検索\n");
 	printf("８：都道府県カナ＋住所カナで探索\n");
+	printf("９：メインメニューに戻る");
 	printf("------------------------------------\n");
 	Gakusei key_gakusei; // 探したい学生キー
 	int key; scanf_s("%d", &key);
-	char buff[300];
-	switch (key) {
-	case 1:
-		printf("学籍番号を入力してください：");
-		int gakuno; scanf("%d", &gakuno);
-		key_gakusei.gakuno = gakuno;
-	case 2:
-		printf("性カナを入力してください：");
-		gets_s(buff, sizeof(buff));
-		strcpy_s(key_gakusei.namesei, buff);
-		break;
-	case 3:
-		printf("名カナを入力してください：");
-		gets_s(buff, sizeof(buff));
-		strcpy_s(key_gakusei.namesei, buff);
-		break;
-	case 4:
-		printf("性カナ、名カナを入力してください：");
-		gets_s(buff, sizeof(buff));
-		strcpy_s(key_gakusei.namesei, buff);
-		break;
-	case 5:
-		printf("郵便番号(前3桁)を入力してください：");
-		gets_s(buff, sizeof(buff));
-		strcpy_s(key_gakusei.namesei, buff);
-		break;
-	case 6:
-		printf("郵便番号(全桁)を入力してください：");
-		gets_s(buff, sizeof(buff));
-		strcpy_s(key_gakusei.namesei, buff);
-		break;
-	case 7:
-		printf("都道府県カナを入力してください：");
-		gets_s(buff, sizeof(buff));
-		strcpy_s(key_gakusei.namesei, buff);
-		break;
-	case 8:
-		printf("都道府県カナ＋住所カナを入力してください：");
-		gets_s(buff, sizeof(buff));
-		strcpy_s(key_gakusei.namesei, buff);
-		break;
-	default:
-		printf("不正なキー識別番号です\n");
-		return;
+	while (scanf_s("%d", &key) != 9) {
+		char buff[300];
+		switch (key) {
+		case 1:
+			printf("学籍番号を入力してください：");
+			int gakuno; scanf("%d", &gakuno);
+			key_gakusei.gakuno = gakuno;
+		case 2:
+			printf("性カナを入力してください：");
+			gets_s(buff, sizeof(buff));
+			strcpy_s(key_gakusei.namesei, buff);
+			break;
+		case 3:
+			printf("名カナを入力してください：");
+			gets_s(buff, sizeof(buff));
+			strcpy_s(key_gakusei.namesei, buff);
+			break;
+		case 4:
+			printf("性カナ、名カナを入力してください：");
+			gets_s(buff, sizeof(buff));
+			strcpy_s(key_gakusei.namesei, buff);
+			break;
+		case 5:
+			printf("郵便番号(前3桁)を入力してください：");
+			gets_s(buff, sizeof(buff));
+			strcpy_s(key_gakusei.namesei, buff);
+			break;
+		case 6:
+			printf("郵便番号(全桁)を入力してください：");
+			gets_s(buff, sizeof(buff));
+			strcpy_s(key_gakusei.namesei, buff);
+			break;
+		case 7:
+			printf("都道府県カナを入力してください：");
+			gets_s(buff, sizeof(buff));
+			strcpy_s(key_gakusei.namesei, buff);
+			break;
+		case 8:
+			printf("都道府県カナ＋住所カナを入力してください：");
+			gets_s(buff, sizeof(buff));
+			strcpy_s(key_gakusei.namesei, buff);
+			break;
+		case 9:
+			return;
+			break;
+		default:
+			printf("不正なキー識別番号です\n");
+			break;
+		}
 	}
 
 	printf("探索アルゴリズムを選択してください。(線形探索：1, 二分探索：2)\n");
@@ -130,11 +164,11 @@ void g_srch_menu(Gakusei *gakusei, int cunt) {
 	switch (method) {
 	case 1:  // 線形探索で先頭から順番にkey_gakusei情報に合致する学生レコードを探す
 		int pos = srch_gakusei_line(key, gakusei, 0, cunt-1, key_gakusei);
-		disp_gakusei(gakusei[pos]); // 検索結果の学生レコード表示
+		disp_gakusei_record(gakusei[pos]); // 検索結果の学生レコード表示
 		break;
 	case 2:  // 二分探索でkey_gakusei情報に合致する学生レコードを探す
 		int pos = srch_gakusei_bin(key, gakusei, 0, cunt-1, key_gakusei);
-		disp_gakusei(gakusei[pos]); // 検索結果の学生レコード表示
+		disp_gakusei_record(gakusei[pos]); // 検索結果の学生レコード表示
 		break;
 	default:
 		printf("1か2で選択しろ\n");
